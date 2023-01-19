a4a2c9258ae07afc58435a7e4f79fa0b742efcfc
Fernando <ferc.dev@gmail.com>
2023-01-19 03:01:16 -0500

    Adapt canvas/resume template to PHP

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/mp/arepa3/';
+$config['base_url'] = 'http://localhost/mp/arepa3/';
 
 /*
 |--------------------------------------------------------------------------

006031c1d77edaa1c0a3052ee09aeb924e99efb0
Fernando <ferc.dev@gmail.com>
2023-01-22 22:06:09 -0500

    Create a login procedure

---
+++ application/controllers/Login.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	public function index()
+	{
+		if ($this->input->server('REQUEST_METHOD') === 'POST') {
+			$data = $this->input->post(NULL, TRUE);
+			if ($data['username']=='fernando' & $data['password']=='password-user') {
+				$_SESSION['user'] = True;
+				redirect(base_url("admin/blog"));
+			}
+		}
+		$this->load->view('public/sections/login/login');
+	}
+}
